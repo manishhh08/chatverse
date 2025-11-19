9a609a1ab3f9e4ed18753773f5964051a36f875e
manishhh08 <100024795+manishhh08@users.noreply.github.com>
2025-11-19 14:31:53 +1100

    css fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./app.css";
+import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
